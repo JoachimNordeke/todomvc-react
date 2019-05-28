ee7c9ee8bf2ea12e2fb0739c0cbc62ad9247e05f
Giss47 <ghassan_aidi@hotmail.com>
2019-05-28 13:14:05 +0200

    Onhashchange fixed

--- src/TodoMVC.js
+++ src/TodoMVC.js
@@ -19,6 +19,7 @@ class TodoMVC extends Component {
     const localState = JSON.parse(localStorage.getItem("todos"));
     if (localState !== null) {
       this.setState(localState);
+      window.onhashchange = this.displayByHash;
     }
   }
 
@@ -55,7 +56,6 @@ class TodoMVC extends Component {
     let newState = this.state;
     newState.viewMode = "all";
     this.setState(newState);
-
     this.saveLocal();
   }
   completedViewMode = () => {
@@ -150,8 +150,26 @@ class TodoMVC extends Component {
     this.saveLocal();
   };
 
-  showAll = () => {
+  displayByHash = () => {
 
+    if (window.location.hash === "#/all") {
+      let newState = this.state;
+      newState.viewMode = "all";
+      this.setState(newState);
+      this.saveLocal();
+    }
+    else if (window.location.hash === "#/active") {
+      let newState = this.state;
+      newState.viewMode = "active";
+      this.setState(newState);
+      this.saveLocal();
+    }
+    else if (window.location.hash === "#/completed" || window.location.hash === "") {
+      let newState = this.state;
+      newState.viewMode = "completed";
+      this.setState(newState);
+      this.saveLocal();
+    }
   }
 }
 
